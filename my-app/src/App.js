10d937543683eed6b5dd9e0d9af6a9f994fdd62e
Joanna Cheng <joannacheng21@outlook.com>
2022-11-02 13:23:30 -0400

    set up fetch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [currFact, setCurrFact] = useState("");
+
+  function fetchFact() {
+    fetch("https://catfact.ninja/fact")
+    .then(response => response.json())
+    .then(data => setCurrFact(data.fact));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header id="title">Cat Facts</header>
+      <p id="currFact">{currFact}</p>
+      <button id="generateButton" onClick={fetchFact}>Generate Fact</button>
     </div>
   );
 }
