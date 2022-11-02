58bc801d67c632f4a8fdc407ecc7d1130843f6fc
Joanna Cheng <joannacheng21@outlook.com>
2022-11-02 16:22:54 -0400

    add fact history

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,27 @@ import { useState } from 'react';
 
 function App() {
   const [currFact, setCurrFact] = useState("");
+  const [prevFacts, setPrevFacts] = useState([]);
 
   function fetchFact() {
     fetch("https://catfact.ninja/fact")
     .then(response => response.json())
-    .then(data => setCurrFact(data.fact));
+    .then((data) => {
+      let pfacts = prevFacts;
+      if (pfacts.length === 0) {
+        pfacts = [currFact];
+      } else {
+        pfacts.unshift(currFact);
+        if (pfacts.length > 10) {
+          pfacts = pfacts.slice(0, -1);
+        }
+      }
+      console.log(pfacts);
+      if (currFact !== "") {
+        setPrevFacts(pfacts);
+      }
+      setCurrFact(data.fact);
+    });
   }
 
   return (
@@ -15,6 +31,7 @@ function App() {
       <header id="title">Cat Facts</header>
       <p id="currFact">{currFact}</p>
       <button id="generateButton" onClick={fetchFact}>Generate Fact</button>
+      <p>{prevFacts.map(f => (<p>{f}</p>))}</p>
     </div>
   );
 }
